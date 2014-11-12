528864a2a6c198567fb851364aa0b882774bf3ba
Alexander Rössler <mail.aroessler@gmail.com>
2014-11-12 02:19:07 +0000

    removed problem in config, replaced start script with Python script

--- run.py
+++ run.py
@@ -6,63 +6,27 @@ import subprocess
 import importlib
 import argparse
 from time import *
-
-
-def rip_environment(path=None, force=False):
-    if os.getenv('EMC2_PATH') is not None:    # check if already ripped
-        return
-
-    if path is None:
-        command = None
-        scriptFilePath = os.environ['HOME'] + '/.bashrc'
-        if os.path.exists(scriptFilePath):
-            with open(scriptFilePath) as f:    # use the bashrc
-                content = f.readlines()
-                for line in content:
-                    if 'rip-environment' in line:
-                        line = line.strip()
-                        if (line[0] == '.'):
-                            command = line
-
-        scriptFilePath = os.environ['HOME'] + '/machinekit/scripts/rip-environment'
-        if os.path.exists(scriptFilePath):
-            command = '. ' + scriptFilePath
-
-        if (command is None):
-            sys.stderr.write('Unable to rip environment\n')
-            sys.exit(1)
-    else:
-        command = '. ' + path + '/scripts/rip-environment'
-
-    process = subprocess.Popen(command + ' && env', stdout=subprocess.PIPE, shell=True)
-    for line in process.stdout:
-        (key, _, value) = line.partition('=')
-        os.environ[key] = value.rstrip()
-
-    sys.path.append(os.environ['PYTHONPATH'])
+from machinekit import launcher
 
 parser = argparse.ArgumentParser(description='starts TheCoolTool configs')
+parser.add_argument('-nm', '--no_machinetalk', help='disables Machinetalk', action='store_true')
 parser.add_argument('config', nargs=1, help='path to config file')
 
 args = parser.parse_args()
 
 configName = args.config[0]
-
-rip_environment()
-launcher = importlib.import_module('machinekit.launcher')
+mtEnabled = not args.no_machinetalk
 
 launcher.register_exit_handler()
-launcher.set_debug_level(5)
+#launcher.set_debug_level(5)
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
 try:
     launcher.check_installation()
     launcher.cleanup_session()
     launcher.load_bbio_file('paralell_cape2.bbio')
-    #launcher.install_comp('gantry.comp')
-    #launcher.install_comp('led_dim.comp')
-    #launcher.install_comp('thermistor_check.comp')
-    launcher.start_process("configserver ../Machineface ../Cetus/")
+    if mtEnabled:
+        launcher.start_process("configserver ../Machineface ../Cetus/")
     launcher.start_process('linuxcnc ' + configName)
 except subprocess.CalledProcessError:
     launcher.end_session()
--- run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-config-pin -f paralell_cape2.bbio
-linuxcnc $1
