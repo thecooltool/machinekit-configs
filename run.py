e86877bd972c0c3ce25bd8edcf14963cf73299ea
Alexander Rössler <mail.aroessler@gmail.com>
2015-05-28 19:43:32 +0200

    updated run.py to make launching of Machinekit configs easier

--- run.py
+++ run.py
@@ -1,28 +1,54 @@
 #!/usr/bin/python
-
 import sys
 import os
 import subprocess
 import argparse
+import tempfile
 from time import sleep
 from machinekit import launcher
 
+if sys.version_info >= (3, 0):
+    import configparser
+else:
+    import ConfigParser as configparser
+
+
 parser = argparse.ArgumentParser(description='starts TheCoolTool configs')
-parser.add_argument('-nm', '--no_machinetalk', help='disables Machinetalk', action='store_true')
+parser.add_argument('-nm', '--no_machinetalk', help='legacy option', action='store_true')
+parser.add_argument('-mk', '--mkwrapper', help='enables mkwrapper mode', action='store_true')
+parser.add_argument('-m', '--machineface', help='displays the Machineface user interface', action='store_true')
+parser.add_argument('-c', '--cetus', help='displays the Cetus user interface', action='store_true')
 parser.add_argument('-r', '--robot', help='enables robot interaction', action='store_true')
 parser.add_argument('config', nargs=1, help='path to config file')
 
 args = parser.parse_args()
 
 configName = args.config[0]
-mtEnabled = not args.no_machinetalk
+mtEnabled = args.mkwrapper
 robotEnabled = args.robot
+machineface = args.machineface
+cetus = args.cetus
 
 launcher.register_exit_handler()
 #launcher.set_debug_level(5)
 if mtEnabled:
     launcher.set_machinekit_ini('machinekit.ini')
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
+
+if not os.path.isfile(configName):
+    sys.stderr.write('Config file %s does not exist' % configName)
+    exit(1)
+
+sourceIni = open(configName)  # open ini
+(tempIniFd, tempIniName) = tempfile.mkstemp()  # create a temporary file
+tempIni = open(tempIniName, 'w')
+tempIni.write(sourceIni.read())  # copy file contents
+if mtEnabled:
+    tempIni.write('DISPLAY = mkwrapper\n')
+else:
+    tempIni.write('DISPLAY = axis\n')
+tempIni.close()
+sourceIni.close()
 
 try:
     launcher.check_installation()
@@ -32,12 +58,27 @@ try:
     else:
         launcher.load_bbio_file('paralell_cape2.bbio')
     if mtEnabled:
-        launcher.start_process("configserver ~/Machineface ~/Cetus/")
-    launcher.start_process('linuxcnc ' + configName)
+        cfg = configparser.ConfigParser({'NAME': ''})
+        cfg.read(tempIniName)
+        machineName = cfg.get('EMC', 'NAME')
+
+        command = 'configserver'
+        if machineName is not '':
+            command += ' -n %s' % machineName
+        if machineface:
+            command += ' ~/Machineface'
+        if cetus:
+            command += ' ~/Cetus'
+        launcher.start_process(command)
+    launcher.start_process('linuxcnc ' + tempIniName)
+    while True:
+        launcher.check_processes()
+        sleep(1)
 except subprocess.CalledProcessError:
     launcher.end_session()
-    sys.exit(1)
+    exit(1)
+finally:
+    tempIni.close()
+    os.remove(tempIniName)
 
-while True:
-    sleep(1)
-    launcher.check_processes()
+exit(0)
